aa6ae32fa657a8fbf1fff50ffd2d1fd7f6aa900f
maggiebarker <maggieb.06@gmail.com>
2017-10-14 20:52:12 -0600

    styling and set-up

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+// Get elements
+
+// Make an object with the questions and answers
+
+// Need a question counter to track the question #
+
+// Array containing user answers
+
+// Need a start button
+
+// Need to display the question/possible answers
+
+// Need to initiate the countdown 20 seconds per question
+
+// Iimmediately move to the answer screen when an answer is selected
+
+// Need to display the correct answer/reaction
+
+// Need to move to the next question after displaying the answer 5 second delay
+
+// Display total right/wrong answers
+
+// Need a restart button
